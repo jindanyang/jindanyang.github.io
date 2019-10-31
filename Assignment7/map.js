0b57d8e8961fb5b17b8c9503e990cc17b3e9e21a
jindanyang <54757245+jindanyang@users.noreply.github.com>
2019-10-31 14:13:39 +0800

    Add files via upload

---
+++ Assignment7/map.js
@@ -0,0 +1,34 @@
+[
+  {
+    "Name": "Mango",
+    "Lat": 22.33028,
+    "Lng": 114.172068,
+    "url": "https://shop.mango.com/preHome.faces",
+    "Pic": "ls.jpg (https://dl.airtable.com/.attachments/f1354b2b46ef951041d7910c22ee984c/c43dbe1a/ls.jpg)",
+    "img_url": "https://dl.airtable.com/.attachments/f1354b2b46ef951041d7910c22ee984c/c43dbe1a/ls.jpg"
+  },
+  {
+    "Name": "ZARA",
+    "Lat": 22.404158,
+    "Lng": 114.189859,
+    "url": "https://www.zara.com/",
+    "Pic": "unnamed.jpg (https://dl.airtable.com/.attachments/bb2f1750aaf2e458ad5c08f415763777/29330a4b/unnamed.jpg)",
+    "img_url": "https://dl.airtable.com/.attachments/bb2f1750aaf2e458ad5c08f415763777/29330a4b/unnamed.jpg"
+  },
+  {
+    "Name": "Uniqlo",
+    "Lat": 22.385909,
+    "Lng": 114.188996,
+    "url": "https://www.uniqlo.com.hk/pc/zh_HK/",
+    "Pic": "uniqlo_butterboom.jpg (https://dl.airtable.com/.attachments/f7da2b2c09be55fc5ebd7340c8e8d148/f0d1fae1/uniqlo_butterboom.jpg)",
+    "img_url": "https://dl.airtable.com/.attachments/f7da2b2c09be55fc5ebd7340c8e8d148/f0d1fae1/uniqlo_butterboom.jpg"
+  },
+  {
+    "Name": "Marks & Spencer",
+    "Lat": 22.386387,
+    "Lng": 114.189511,
+    "url": "https://www.marksandspencer.com/hk/?extid=ps_ggl_4PS_HK_ALL_PureBrand_MarksandSpencer_EN_Standard&kwid=_Google+Adwords&s_kwcid=AL!2750!3!291538654704!e!!g!!marks%20and%20spencer%20hk&device=c&gclid=EAIaIQobChMIwoPn6s2i5AIV06qWCh2lvQ7rEAAYASAAEgItjPD_BwE",
+    "Pic": "3.2.2.44-Marks-Spencer_03a.jpg (https://dl.airtable.com/.attachments/cc4b3e35d24df03985b5fdfe845113ea/b452e8e7/3.2.2.44-Marks-Spencer_03a.jpg)",
+    "img_url": "https://dl.airtable.com/.attachments/cc4b3e35d24df03985b5fdfe845113ea/b452e8e7/3.2.2.44-Marks-Spencer_03a.jpg"
+  }
+]
